44411e35bd2969abab8072c0f523f43f244aa2ac
Mario Hafer <radolan2map@e.mail.de>
2024-07-26 22:30:54 +0200

    refactor: restructured, added tests

---
+++ tests/test_tests.py
@@ -0,0 +1,39 @@
+from vcf_to_ics import create_vcard, Vcard
+
+card_ok = """BEGIN:VCARD
+VERSION:3.0
+BDAY:1993-06-28
+FN:Weather Mann
+N:Mann;Weather;;;
+UID:3cf6bd87-dab0-4275-8790-4eaa205e1cad
+END:VCARD
+"""
+
+card_without_bday = """BEGIN:VCARD
+VERSION:3.0
+FN:Weather Mann
+N:Mann;Weather;;;
+UID:3cf6bd87-dab0-4275-8790-4eaa205e1cad
+END:VCARD
+"""
+
+
+def test_should_create_vcard():
+    vcard = create_vcard(card_ok)
+    assert vcard.name == "Weather Mann"
+    assert vcard.uid == "3cf6bd87-dab0-4275-8790-4eaa205e1cad"
+
+
+def test_should_not_create_vcard():
+    """without BDAY cannot create vcard"""
+    vcard = create_vcard(card_without_bday)
+    assert vcard is None
+
+
+def test_without_uid():
+    lines = card_ok.split("\n")
+    lines_without_uid = [line for line in lines if not line.startswith("UID")]
+    new_vcard_input = "\n".join(lines_without_uid)
+    vcard = create_vcard(new_vcard_input)
+    # print("###", vcard.uid)
+    assert vcard.uid.startswith("19930628")
--- vcf_to_ics.py
+++ vcf_to_ics.py
@@ -8,7 +8,6 @@ import random
 import time
 import sys
 from datetime import datetime
-from typing import List
 
 # ##### BEGIN GPL LICENSE BLOCK #####
 #
@@ -35,118 +34,129 @@ PROGRAM_VERSION = "1.0"
 
 
 @dataclass
-class VCF2ICS:
-    vcf_file: Path
+class Vcard:
+    """holds elements of a vcard as information for ics file"""
+    name: str
+    dt_birth_start: datetime | None = None
+    dt_birth_end: datetime | None = None
+    birth_start_str = str | None
+    uid: str | None = None
+    age: int | None = None
 
-    def process(self) -> list[str] | None:
-        # Read VCF file content
-        file_content = self.vcf_file.read_text(encoding="utf-8")
-        # Separate VCards
-        all_cards = file_content.split("END:VCARD")
-        print(f"{len(all_cards)} VCards found")
-        if not all_cards:
-            return None
-
-        list_formatted_entries = []
-        for card in all_cards:
-            dict_card = self._get_dict(card)
-            if not dict_card:
-                continue
-            entry = self._parse_vcard_get_formatted_entry(dict_card)
-            list_formatted_entries.append(entry)
-        # for
-        print(f"\n{len(list_formatted_entries)} usable entries")
-        return list_formatted_entries
-
-    def _get_dict(self, card: str) -> dict[str] | None:
-        _dict = {}
-        for line in card.split("\n"):
-            elems = line.split(":")
-            key = elems[0]
-            if key in ("BDAY", "N", "FN", "UID"):
-                _dict[key] = "".join(elems[1:])
-        try:
-            bday = _dict["BDAY"]
-        except KeyError:
-            return None  # not usable
-
-        _dict["BDAY"] = bday.split("T")[0]  # '2000-07-01T000000'
-
-        try:
-            _dict["FN"]  # -> usable
-        except KeyError:
-            try:
-                name = _dict["N"]
-                elems = name.split(";")
-                if elems[3]:
-                    new_name = f"{elems[3]} {elems[1]} {elems[0]}"
-                else:
-                    new_name = f"{elems[1]} {elems[0]}"
-                _dict["FN"] = new_name
-            except KeyError:  # no information
-                _dict = None
-            else:
-                del _dict["N"]
-
-        return _dict
-
-    def _parse_vcard_get_formatted_entry(self, vcard: dict[str]) -> str:
+    def set_birthday(self, bday):
         # BDAY format:
         # BDAY:2000-07-01T00:00:00
         # or   2000-07-01
-        bday = vcard["BDAY"]
+        bday = bday.split("T")[0]  # '2000-07-01T000000'
+        self.birth_start_str = bday.replace("-", "")
         year, month, day = bday.split("-")
-        dt_birth_start = datetime(int(year), int(month), int(day))
-        dt_birth_end = datetime(int(time.strftime("%Y")), int(month), int(day))  # current year!
-        name = vcard["FN"]
+        self.dt_birth_start = datetime(int(year), int(month), int(day))
+        self.dt_birth_end = datetime(int(time.strftime("%Y")), int(month), int(day))  # current year!
 
-        dt_diff = dt_birth_end - dt_birth_start
+        dt_diff = self.dt_birth_end - self.dt_birth_start
         days = dt_diff.days + 1  # ein Tag zuwenig
-        years = int(days / 365)
-        # print(f"{name}: {dt_birth_start} {dt_birth_end} {years}")
-        print(f"{name}: {years}")
+        self.age = int(days / 365)
 
-        birth_start_str = dt_birth_start.strftime("%Y%m%d")
-        try:
-            uid = vcard["UID"]
-        except KeyError:  # not a problem, can be generated later
-            uid = self._generate_uid(birth_start_str)
+    def __str__(self):
+        return f"{self.name}: {self.dt_birth_start} {self.dt_birth_end} {self.age}"
 
-        list_block = [f"BEGIN:VEVENT", f"DTSTART:{birth_start_str}", f"SUMMARY:{name} ({years})",
-                      "RRULE:FREQ=YEARLY", "DURATION:P1D", f"UID:{uid}", "END:VEVENT"]
+    @property
+    def entry(self):
+        list_block = [f"BEGIN:VEVENT", f"DTSTART:{self.birth_start_str}", f"SUMMARY:{self.name} ({self.age})",
+                      "RRULE:FREQ=YEARLY", "DURATION:P1D", f"UID:{self.uid}", "END:VEVENT"]
         entry = "\n".join(list_block)
         return entry
 
-    def _generate_uid(self, birth_start_str: str):
-        """Unique ID
-        The UID purpose is to define a unique identifier across all the calendar components.
-        It is basically a random generated sequence that will assure the uniqueness of every
-        calendar component. This property is mandatory in defining calendar components.
-        Everyone must assure that this unique identifier is added at the Creation Time
-        of his calendar component, to be sure of the uniqueness of all the components.
 
-        Consequences of not doing it right
-        Because the UID globally identifies a calendar component, not having a good way of
-        generating this property can cause a lot of problems. Using a simple UID generation
-        algorithm might lead to overridden events with the same UID generated by others
-        using the same generation mechanism. Every creation of a calendar component with
-        a UID that is already contained by other component will lead in the end to the update
-        of the older component. Once generated, the calendar component should keep the same
-        UID all the time, so that no matter which part of the event is changed, the UID-Reference
-        is the same and the calendar can be updated until that event will be deleted. """
-        uid = ''.join([random.choice(list(ascii_letters + digits)) for _ in range(16)]) + "@VCFtoICS.com"
-        return f"{birth_start_str}-{uid}"
+def create_vcard(card: str) -> Vcard | None:
+    working_dict = {}
+    for line in card.split("\n"):
+        elems = line.split(":")
+        key = elems[0]
+        if key in ("BDAY", "N", "FN", "UID"):
+            working_dict[key] = "".join(elems[1:])
+    try:
+        bday = working_dict["BDAY"]
+    except KeyError:
+        return  # not usable
 
-    def write_ics_file(self, ics_file: Path, calendar_name: str, list_formatted_entries: list[str]):
-        with ics_file.open("w") as f:
-            # Write ICS calendar header
-            f.write(f"BEGIN:VCALENDAR\nPRODID:-//{PROGRAM_NAME}//NONSGML {calendar_name} V1.0//EN\n"
-                    f"X-WR-CALNAME:{calendar_name}\nVERSION:2.0\n")
-            for entry in list_formatted_entries:
-                f.write(f"{entry}\n")
-            # Write ICS calendar footer
-            f.write("END:VCALENDAR\n")
-            print("\n->", ics_file)
+    try:
+        name = working_dict["FN"]  # -> usable
+    except KeyError:
+        try:
+            name = working_dict["N"]
+            elems = name.split(";")
+            if elems[3]:
+                new_name = f"{elems[3]} {elems[1]} {elems[0]}"
+            else:
+                new_name = f"{elems[1]} {elems[0]}"
+            name = new_name
+        except KeyError:  # no information
+            return
+
+    vcard = Vcard(name)
+    vcard.set_birthday(bday)
+    try:
+        vcard.uid = working_dict["UID"]
+    except KeyError:  # not a problem, can be generated later
+        birth_start_str = vcard.dt_birth_start.strftime("%Y%m%d")
+        vcard.uid = generate_uid(birth_start_str)
+
+    return vcard
+
+
+def generate_uid(birth_start_str: str):
+    """Unique ID
+    The UID purpose is to define a unique identifier across all the calendar components.
+    It is basically a random generated sequence that will assure the uniqueness of every
+    calendar component. This property is mandatory in defining calendar components.
+    Everyone must assure that this unique identifier is added at the Creation Time
+    of his calendar component, to be sure of the uniqueness of all the components.
+
+    Consequences of not doing it right
+    Because the UID globally identifies a calendar component, not having a good way of
+    generating this property can cause a lot of problems. Using a simple UID generation
+    algorithm might lead to overridden events with the same UID generated by others
+    using the same generation mechanism. Every creation of a calendar component with
+    a UID that is already contained by other component will lead in the end to the update
+    of the older component. Once generated, the calendar component should keep the same
+    UID all the time, so that no matter which part of the event is changed, the UID-Reference
+    is the same and the calendar can be updated until that event will be deleted. """
+    uid = ''.join([random.choice(list(ascii_letters + digits)) for _ in range(16)]) + "@VCFtoICS.com"
+    return f"{birth_start_str}-{uid}"
+
+
+def process() -> list[str] | None:
+    # Read VCF file content
+    file_content = vcf_file.read_text(encoding="utf-8")
+    # Separate VCards
+    all_cards = file_content.split("END:VCARD")
+    print(f"{len(all_cards)} VCards found")
+    if not all_cards:
+        return None
+
+    formatted_entries = []
+    for card in all_cards:
+        vcard = create_vcard(card)
+        if not vcard:
+            continue
+        print(f"- {vcard.birth_start_str}\t{vcard.age}\t{vcard.name}")
+        formatted_entries.append(vcard.entry)
+    # for
+    print(f"\n{len(formatted_entries)} usable entries")
+    return formatted_entries
+
+
+def write_ics_file(ics_file: Path, calendar_name: str, list_formatted_entries: list[str]):
+    with ics_file.open("w") as f:
+        # Write ICS calendar header
+        f.write(f"BEGIN:VCALENDAR\nPRODID:-//{PROGRAM_NAME}//NONSGML {calendar_name} V1.0//EN\n"
+                f"X-WR-CALNAME:{calendar_name}\nVERSION:2.0\n")
+        for entry in list_formatted_entries:
+            f.write(f"{entry}\n")
+        # Write ICS calendar footer
+        f.write("END:VCALENDAR\n")
+        print("\n->", ics_file)
 
 
 if __name__ == "__main__":
@@ -165,11 +175,9 @@ if __name__ == "__main__":
         print("Invalid input file path:", vcf_file)
         sys.exit(1)
 
-    vcf2ics = VCF2ICS(vcf_file)
-    list_formatted_entries = vcf2ics.process()
-
-    if not list_formatted_entries:
+    formatted_entries = process()
+    if not formatted_entries:
         sys.exit(1)
 
     # Write ICS event:
-    vcf2ics.write_ics_file(ics_file, calendar_name, list_formatted_entries)
+    write_ics_file(ics_file, calendar_name, formatted_entries)
